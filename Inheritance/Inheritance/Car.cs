7cd8c2851cbba21b193f94eb6dd92b6179b1e7d8
Noel Mowatt <nwmowatt@fullsail.edu>
2016-11-27 12:41:05 -0500

    Inheritance pj - Setup
    Created 2 classes with one being a subclass of the other.

---
+++ Inheritance/Inheritance/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Car
+    {
+        public readonly int NumOfWheels;
+        public readonly int NumOfSeats;
+
+        //--constructor
+        public Car(int numOfWheels, int numOfSeats)
+        {
+            NumOfWheels = numOfWheels;
+            NumOfSeats = numOfSeats;
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Ferrari.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Ferrari : Car
+    {
+        //--fields unique to Ferrari
+        public readonly int Speeds;
+        public readonly string Color;
+
+        //--Subclass Ferrari inherits from Car class
+        public Ferrari(int numOfWheels, int numOfSeats, int speeds, string color) : base(numOfWheels, numOfSeats)
+        {
+            Speeds = speeds;
+            Color = color;
+        }
+    }
+}
