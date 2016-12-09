b2bfe0de82e6a72e11b1ac61b235cad3ab0e03b1
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-09 17:00:44 -0500

    Trimmed class member fields to one field

--- MansBestFriend/MansBestFriend/Dog.cs
+++ MansBestFriend/MansBestFriend/Dog.cs
@@ -8,6 +8,17 @@ namespace MansBestFriend
 {
     class Dog : Animal
     {
-        private int dogYears;
+        private bool IsTrained;
+
+        public Dog(bool isTrained) : base(typee:"?",age:0, name:"?", gender:'?')
+        {
+            IsTrained = isTrained;
+        }
+
+        public bool Trained_GS
+        {
+            get { return IsTrained; }
+            set { IsTrained = value; }
+        }
     }
 }
