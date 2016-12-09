82c63b72cb068a53634be4b256e0c87fc3cc0e76
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-09 10:40:58 -0500

    Minor change in Brief Desc in the comments

--- MansBestFriend/MansBestFriend/Program.cs
+++ MansBestFriend/MansBestFriend/Program.cs
@@ -13,7 +13,7 @@ namespace MansBestFriend
             /*  Noel Mowatt, Jr.
                 Date: 12 - 09 - 2016
                 Project: Man's Best Friend  
-                Brief Desc: User chooses a 1 of 3 animals for a pet - Monkey, Cat, or Dog*/
+                Brief Desc: User chooses a one of three animals for a pet - Monkey, Cat, or Dog*/
         }
     }
 }
