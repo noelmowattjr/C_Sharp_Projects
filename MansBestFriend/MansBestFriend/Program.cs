81482f4adea41c1fa2c307e3c493802303cf0901
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-09 10:38:19 -0500

    New project initiated

---
+++ MansBestFriend/MansBestFriend/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MansBestFriend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Noel Mowatt, Jr.
+                Date: 12 - 09 - 2016
+                Project: Man's Best Friend  
+                Brief Desc: User chooses a 1 of 3 animals for a pet - Monkey, Cat, or Dog*/
+        }
+    }
+}
