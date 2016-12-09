60816527bde137abb7723150b99bd0cfaaaa4ff7
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-09 16:59:30 -0500

    Added additional class fields to Animal class

--- MansBestFriend/MansBestFriend/Animal.cs
+++ MansBestFriend/MansBestFriend/Animal.cs
@@ -8,7 +8,41 @@ namespace MansBestFriend
 {
     class Animal
     {
-        private string type;
-        private int age;
+        //--Animal member fields
+        private string Typee;
+        private int Age;
+        private string Name;
+        private char Gender;
+
+        //--Animal Constructor
+        public Animal(string typee, int age, string name, char gender)
+        {
+            Typee = typee; Age = age; Name = name; Gender = gender;
+        }
+
+        //------Getters - Setters-------------//
+        public string Type_Animal_GS
+        {
+            get { return Typee; }
+            set { Typee = value; }
+        }
+
+        public int Age_GS
+        {
+            get { return Age; }
+            set { Age = value; }
+        }
+
+        public string Name_GS
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public char Gender_GS
+        {
+            get { return Gender; }
+            set { Gender = value; }
+        }
     }
 }
