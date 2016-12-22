e2f5806f61c9563701ace1df9569a04b32421cd2
Noel Mowatt <NoelMowattJr@gmail.com>
2016-12-22 09:37:55 -0500

    New FooBar class created - Two Interfaces created both of which define the same method

---
+++ Interfaces_101/Interfaces_101/FooBar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_101
+{
+    //--Created 2 Interfaces that define the same method--//
+    interface IFoo
+    {
+        void SomeMethod();
+    }
+
+    interface IBar
+    {
+        void SomeMethod();
+    }
+
+    //--Class FooBar - implementing the 2 Interfaces above
+    class FooBar : IFoo, IBar
+    {
+        //--Class method
+        public void SomeMethod()
+        {
+            Console.WriteLine("This is the class method!");
+        }
+    }
+}
